2bd5c6f4710d1ef5491b07dd3cbce07575dd8855
nprigent <nprigent@student.42.fr>
2015-07-03 11:43:19 +0200

    Ajout de la fonction _save au storage service ainsi que le retour du ls

--- www/services/storageService.js
+++ www/services/storageService.js
@@ -3,17 +3,27 @@
 
 	define([
 	], function() {
+
 		var storage = {
 			getKey: getKey
 		};
+
 		return storage;
+
 		function getKey(key) {
 			var wrapper = {
-				key: key,
-				obj: {},
-				save: function () {}
-			}
+				key:  key,
+				obj:  {},
+				save: _save
+			};
+
+			wrapper.obj = localStorage.getItem(key);
+
 			return wrapper;
+		}
+
+		function _save() {
+			localStorage.setItem(this.key, this.obj);
 		}
 	});
 
