a8494972e3a495c6e5239843fd9161ddf3c9046c
Joachim Westphal <elindorath@gmail.com>
2015-07-03 11:21:09 +0200

    created branch

--- www/services/storageService.js
+++ www/services/storageService.js
@@ -6,15 +6,6 @@
 
 		return storage;
 
-		function storage(key, value) {
-			if (arguments.length == 0)
-				localStorage.clear();
-			else if (arguments.length == 1)
-				return localStorage[key];
-			else
-				localStorage[key] = value;
-		}
-
 	});
 
 })();
