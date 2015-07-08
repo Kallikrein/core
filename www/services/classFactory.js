560e5d321cf44983e24e97ca54d5b374fa7834fa
nprigent <nprigent@student.42.fr>
2015-07-08 18:00:57 +0200

    applying populate, populateAll, save modification

--- www/services/classFactory.js
+++ www/services/classFactory.js
@@ -3,144 +3,104 @@
 
 	define([
 		'm',
+		'bluebird',
 		'tools',
-		'bluebird'
-	], function(m, T, Promise) {
+		'services/localStorageService'
+	], function(m, Promise, T, storage) {
 
 		return classFactory;
 
 		function classFactory(model) {
-			var Class = function() {
+			var namespace = {
+				create: create
+			};
+
+			var Class = function(attributes) {
 				var self = this;
-				var attributes = model.attributes;
 
+				attributes = attributes || [];
+
+				for (var name in model.attributes) {
+					self[name] = getterSetter(attributes[name]);
+				}
+
+			};
+
+			Class.prototype.update = function(attributes) {
 				for (var name in attributes) {
-					// Verify here required value ?
-					self[name] = getterSetterFactory(attributes[name]);
-				}
-
-			};
-
-			Class.prototype.save = function() {};
-
-			Class.prototype.populate = function(obj) {
-				for (var key in obj) {
-					if (this[key] !== undefined)
-						this[key](obj[key]);
+					this[name](attributes[name]);
 				}
 			};
 
-			return Class;
-		}
+			Class.prototype.save = function() {
+				var saveModel = {};
 
-		function getterSetterFactory(attribute) {
-			var stored;
+				for (var key in model.attributes) {
+					if (model.attributes[key].collection)
+						saveModel[key] = T.pluck(this[key](), 'id');
+					else if (model.attributes[key].model)
+						saveModel[key] = this[key]().id || this[key];
+					else
+						saveModel[key] = this[key]();
+				}
 
-			function getterSetter(value) {
-				if (arguments.length)
-					stored = value;
-				return stored;
+				return storage.store(saveModel);
+
+			};
+
+			Class.prototype.populateAll = function() {
+				var self = this;
+
+				return (
+					new Promise(function (resolve) {
+						for (var key in model.attributes) {
+							self.populate(key);
+						}
+
+						resolve(self);
+					})
+				);
+			};
+
+			Class.prototype.populate = function(key) {
+				var tmp = [];
+				var self = this;
+
+				return (
+					new Promise(function (resolve, reject) {
+						if (model.attributes[key].collection || model.attributes[key].model) {
+							this[key]().map(storage.get).then(tmp.push, reject);
+							this[key](tmp);
+						} else
+							reject('Populate is not possible on ' + key);
+
+						resolve(self);
+					})
+				);
+			};
+
+			function create(attributes) {
+				return new Promise(function(resolve, reject) {
+					for (var name in model.attributes) {
+						if (model.attributes[name].required && attributes[name] === undefined)
+							reject('Attributes ' + name + ' is required');
+					}
+					resolve(new Class(attributes));
+				});
 			}
 
-			getterSetter.isAllowed = attribute.isAllowed || returnTrue;
-			getterSetter.isValid = function(value) {
-				var isValid = attribute.isValid || returnTrue;
+			return namespace;
+		}
 
-				if (isValid(value)) {
-					stored = value;
-					return true;
-				} else
-					return false;
+		function getterSetter(store) {
+			var prop = function(/*value*/) {
+				if (arguments.length)
+					store = arguments[0];
+				return store;
 			};
-
-			return getterSetter;
-		}
-
-		function returnTrue() {
-			return true;
-		}
-
-		function save() {
-			var saveModel = {};
-
-			return (
-				new Promise(function (resolve, reject) {
-					for (var key in attributes) {
-						if (attributes[key].collection)
-							saveModel[key] = T.pluck(this[key](), 'id');
-						else if (attributes[key].model)
-							saveModel[key] = this[key]().id || this[key];
-						else
-							saveModel[key] = this[key]();
-					}
-
-					storage.store(saveModel)
-					.then(function (response) {
-						resolve(this);
-					})
-					.catch(reject);
-				})
-			);
-		}
-
-		function populate(key) {
-			var values = [];
-			var tmp = [];
-
-			return (
-				new Promise(function (resolve, reject) {
-					if (attributes[key].collection || attributes[key].model) {
-						Promise.map(this[key], function (id) {
-							return storage.get(val);
-						})
-						.then(function (values) {
-							console.log(values);
-							this[key](values);
-						})
-						.catch(function (e) {
-							console.log(e);
-							reject(e);
-						})
-						// this[key]().map(function (val) {
-						// 	values.push(storage.get(val));
-						// });
-
-						// values.
-						// this[key](tmp);
-					} else
-						reject('The key is not a collection nor a model, cannot populate');
-
-					resolve(this);
-				})
-			);
-		}
-
-		function populateAll() {
-			var tmp = [];
-
-			return (
-				new Promise(function (resolve, reject) {
-					for (var key in attributes) {
-						if (attributes[key].collection || attributes[key].model) {
-							this[key]().map(function (val) {
-								storage.get(val).then(tmp.push, reject);
-							});
-							this[key](tmp);
-						}
-					}
-
-					resolve(this);
-				})
-			);
+			return prop;
 		}
 
 	});
 
 })();
-
-/*
-
-[1] = [{id: 1}]
-[1, 2] = [{id: 1, name: 'toto'}, {id: 2, name: 'tata'}]
-
- */
