8ea8726bd877a36c4bfab2e62c33912759f940cf
Thomas Charlat <thomas.charlat@gmail.com>
2015-07-13 17:29:47 +0200

    co implementing localStorage

--- www/pages/templatePage.js
+++ www/pages/templatePage.js
@@ -10,14 +10,24 @@
 			controller: function() {
 				var self = this;
 				this.template = new templateModel();
+				this.load = function () {
+					templateModel.get()
+					.then(function (tmp) {
+						console.log(tmp.attribute1());
+					});
+				}
+				this.template.test();
 				this.vm = {
 					globalError: m.prop(''),
 					attribute1Error: m.prop(''),
 					attribute2Error: m.prop(''),
 				};
 				this.submit = function () {
-					self.template.is.valid().then()
-					.catch(self.vm.globalError);
+					self.template.is.valid().then(function (){
+						self.template.save();
+						console.log('saved');
+					});
+					//.catch(self.vm.globalError);
 				}
 			},
 			view: function(c) {
@@ -31,7 +41,11 @@
 					m('button', {
 						onclick: c.submit
 					},
-					'click me'),
+					'save'),
+					m('button', {
+						onclick: c.load
+					},
+					'load'),
 					m.component(templateComponent)
 				]);
 			}
